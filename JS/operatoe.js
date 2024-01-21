636b3f682eb56213dbcce517accd6d192124df12
Abinayaaselvaraj <abinayaselvaraj2503@gmail.com>
2024-01-21 21:08:25 +0530

    this is java script

---
+++ JS/operatoe.js
@@ -0,0 +1,10 @@
+//Assignment operater
+var a=10;
+console.log("Assignment operater:",a)
+//Arithmetic operator
+var ar=20,br=10;
+console.log("Assignment operater:",ar+br)
+//Logical operator
+console.log("Logical operater:",ar>br)
+//Relational operator
+console.log("Relational soperater:",ar>br)
---
+++ JS/variables.js
@@ -0,0 +1,11 @@
+a='a'
+console.log("general value",a)
+var b="var"
+console.log("declared in varaiable:",b)
+let c="let"
+console.log("declared in let:",c)
+const d=600
+console.log("declared in const",d)
+
+//d=800 //remove this to get the output
+console.log("declared in const",d)
